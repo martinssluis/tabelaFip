734f63e2b714e1f45cf46c20bcad64e6b779c19c
Luis Martins <luishenrique.martinslima@gmail.com>
2026-09-19 19:23:48 -0300

    feat(TabelaFipApplication): implementa CommandLineRunner e adiciona metodo run

--- src/main/java/com/martinsluis/tabelaFip/TabelaFipApplication.java
+++ src/main/java/com/martinsluis/tabelaFip/TabelaFipApplication.java
@@ -1,13 +1,21 @@
 package com.martinsluis.tabelaFip;
 
+import com.martinsluis.tabelaFip.Principal.Principal;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TabelaFipApplication {
+public class TabelaFipApplication implements CommandLineRunner { //para o usuário interagir por meio do terminal
 
 	public static void main(String[] args) {
 		SpringApplication.run(TabelaFipApplication.class, args);
+	}
+
+	@Override
+	public void run(String ... args) throws Exception{
+		Principal principal = new Principal();
+		principal.exibirMenu();
 	}
 
 }
