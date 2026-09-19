5c9d46eddaf396e59bb4b787da52a9b5b3a3c5c1
Luis Martins <luishenrique.martinslima@gmail.com>
2026-09-19 19:32:06 -0300

    feat(Principal): cria classe principal que pergunta qual tipo de veiculo usuário deseja para obter informações baseada no tipo do veiculo

---
+++ src/main/java/com/martinsluis/tabelaFip/Principal/Principal.java
@@ -0,0 +1,67 @@
+package com.martinsluis.tabelaFip.Principal;
+
+import com.martinsluis.tabelaFip.config.FipeClientConfig;
+
+import com.martinsluis.tabelaFip.service.ConverteDados;
+
+import java.util.Scanner;
+
+public class Principal {
+    private Scanner scanner = new Scanner(System.in);
+    private FipeClientConfig fipeClientConfig = new FipeClientConfig();
+    private ConverteDados conversor = new ConverteDados();
+    private String tipoVeiculo;
+
+    private final String BASE_URL = "https://parallelum.com.br/fipe/api/v1/";
+
+
+    public void exibirMenu(){
+                 System.out.print("""
+                        ******OPÇÕES******
+                    
+                        (1) Carro
+                        (2) Moto
+                        (3) Caminhão
+                    
+                         Digite uma das opções para consultar valores: 
+                    """);
+            int escolhaTipoVeiculo = scanner.nextInt();
+
+            switch (escolhaTipoVeiculo) {
+                case 1:
+                    System.out.println("Carro");
+                    tipoVeiculo = "carros";
+                    break;
+
+                case 2:
+                    System.out.println("Moto");
+                    tipoVeiculo = "motos";
+                    break;
+
+                case 3:
+                    System.out.println("Caminhão");
+                    tipoVeiculo = "caminhoes";
+                    break;
+
+                default:
+                    System.out.println("Opção inválida");
+            }
+
+            var urlMarcas = BASE_URL + tipoVeiculo + "/marcas";
+        System.out.println(urlMarcas);
+
+            var jsonTipoVeiculo = fipeClientConfig.getData(urlMarcas);
+        System.out.println("jsonTipoVeiculo: "+ jsonTipoVeiculo);
+
+
+
+
+        }
+
+        // caso o usuário escreva uma opção inválida, deve lançar exceção
+
+        //* Escolher marca do carro pelo código
+        //* Digitar trecho do nome do carro para consulta
+        //* Digitar código do modelo para consultar valores
+        //* Mostrar os veículos de a cordo com o ano
+    }
