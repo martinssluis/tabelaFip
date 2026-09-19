3a991bfdf54ebf4b6c44302192fd645d81b26870
Luis Martins <luishenrique.martinslima@gmail.com>
2026-09-19 19:28:21 -0300

    feat(FipeClienteConfig): cria classe que faz conexão com api de forma generica

---
+++ src/main/java/com/martinsluis/tabelaFip/config/FipeClientConfig.java
@@ -0,0 +1,34 @@
+package com.martinsluis.tabelaFip.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class FipeClientConfig {
+
+    public String getData(String endereco){
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+
+        HttpResponse<String> response = null;
+        try {
+            response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+        }catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        String json = response.body();
+        return json;
+    }
+}
