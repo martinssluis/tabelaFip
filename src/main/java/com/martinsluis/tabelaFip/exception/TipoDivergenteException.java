f55b7ed788a92846d62b317e5adde3e0631962b6
Luis Martins <luishenrique.martinslima@gmail.com>
2026-09-24 22:48:36 -0300

    feat(TipoDivergenteException): cria exceção dedicada

---
+++ src/main/java/com/martinsluis/tabelaFip/exception/TipoDivergenteException.java
@@ -0,0 +1,7 @@
+package com.martinsluis.tabelaFip.exception;
+
+public class TipoDivergenteException extends RuntimeException {
+    public TipoDivergenteException(String message) {
+        super(message);
+    }
+}
